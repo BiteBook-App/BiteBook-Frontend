f002d91ef3fb939e01f3244ef2dc53ca72ccb87d
michelletaing <mleetaing@gmail.com>
2025-02-24 16:13:50 -0500

    added gluestack components

---
+++ components/ui/custom-modal/index.tsx
@@ -0,0 +1,64 @@
+import { View } from "react-native";
+import { useRouter } from 'expo-router';
+import { Text } from "@/components/ui/text";
+import {
+  Modal,
+  ModalBackdrop,
+  ModalContent,
+  ModalCloseButton,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "@/components/ui/modal"
+import { Heading } from "@/components/ui/heading";
+import { Button, ButtonText } from "@/components/ui/button";
+import { Icon, CloseIcon, ArrowLeftIcon } from "@/components/ui/icon";
+import { HStack } from "@/components/ui/hstack";
+import { Pressable } from "react-native";
+
+interface CustomModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export default function CustomModal({ isOpen, onClose }: CustomModalProps) {  
+    const router = useRouter();
+  
+    return (
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        size="lg"
+      >
+        <ModalBackdrop />
+        <ModalContent>
+          <ModalHeader>
+            <Heading size="lg" className="text-typography-950">
+                Email successfully delivered!
+            </Heading>
+          </ModalHeader>
+          <ModalBody>
+            <Text size="md" className="text-typography-500">
+                Check your inbox for instructions on how to reset your password.
+            </Text>
+          </ModalBody>
+          <ModalFooter className="justify-center">
+            <Pressable onPress={() => router.back()}>
+              <HStack 
+                space="sm" 
+                className="justify-center"
+              >
+                <Icon 
+                  as={ArrowLeftIcon} 
+                  className="text-background-400"
+                />
+                <Text className="text-background-400">
+                  Back to sign in
+                </Text>
+              </HStack>
+            </Pressable>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    );
+}
