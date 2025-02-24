2a25862028dcf228a9f768cc4876d72217b0a498
michelletaing <mleetaing@gmail.com>
2025-02-24 16:14:56 -0500

    added dark mode styling

--- app/home.tsx
+++ app/home.tsx
@@ -1,17 +1,19 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import { Link } from 'expo-router';
+import { Text } from "@/components/ui/text";
 
 export default function Home() {
   return (
     <View
+      className="bg-background-dark px-5 lg:px-40"
       style={{
         flex: 1,
         justifyContent: "center",
-        alignItems: "center",
+        width: "100%", 
       }}
     >
       <Text>This is the home page</Text>
-      <Link href="/recipe">
+      <Link href="/recipe" className="text-typography-800">
         Go to a recipe
       </Link>
     </View>
