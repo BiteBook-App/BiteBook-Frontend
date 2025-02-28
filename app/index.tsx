e3cc78f0af57ddcc37d70ef98422f9e62774c6f1
skaf.lucas <skaf.lucas.cel@gmail.com>
2025-02-27 21:03:57 -0500

    Add handlePhoneSignIn to bring user to Phone Sign In page

--- app/index.tsx
+++ app/index.tsx
@@ -6,16 +6,17 @@ import { useEffect, useState } from 'react';
 import { Text } from "@/components/ui/text";
 import { FormControl, FormControlError, FormControlErrorIcon, FormControlErrorText } from "@/components/ui/form-control";
 import { VStack } from "@/components/ui/vstack";
-import { MailIcon, LockIcon, CloseCircleIcon } from "@/components/ui/icon";
+import {MailIcon, LockIcon, CloseCircleIcon, PhoneIcon, createIcon} from "@/components/ui/icon";
 import { Image } from "@/components/ui/image";
 import { HStack } from "@/components/ui/hstack";
-import { Button, ButtonText } from "@/components/ui/button";
+import {Button, ButtonIcon, ButtonText} from "@/components/ui/button";
 import { LinearGradient } from 'expo-linear-gradient';
-import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { useRouter, Link } from "expo-router";
 import { Spinner } from "@/components/ui/spinner"
 import CustomInputField from "@/components/ui/custom-input-field"
+import { app, auth } from "@/config/firebase"
+import {Path} from "react-native-svg";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -51,21 +52,6 @@ export default function Login() {
     return null;
   }
 
-  // Firebase config - TO DO: Move somewhere else?
-  const firebaseConfig = {
-    apiKey: process.env.EXPO_PUBLIC_API_KEY,
-    authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
-    databaseURL: process.env.EXPO_PUBLIC_DATABASE_URL,
-    projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.EXPO_PUBLIC_APP_ID,
-    measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID
-  };
-
-  const app = initializeApp(firebaseConfig);
-
-  const auth = getAuth();
   const handleSignIn = () => {
     setLoading(true);
     signInWithEmailAndPassword(auth, email, password)
@@ -84,53 +70,74 @@ export default function Login() {
       });
   };
 
-  return (
-    <View
-      className="bg-background-dark px-5 lg:px-40"
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        width: "100%", 
-      }}
-    >
-      <LinearGradient
-        colors={[
-          "#232d37", "#232b34", "#222832", "#22262f", "#21242c",
-          "#202229", "#1f2027", "#1e1e24", "#1d1c21", "#1b1a1e",
-          "#1a191c", "#181719"
-        ]}
-        locations={[0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.7]}
-        start={{ x: 1, y: 0 }}
-        end={{ x: 0, y: 1 }}
-        style={{
-          position: 'absolute',
-          left: 0,
-          right: 0,
-          top: 0,
-          bottom: 0, // Ensures full height
-          justifyContent: 'center', // Centers content vertically
-          alignItems: 'center', // Centers content horizontally
-        }}
+  const handlePhoneSignIn = async (phoneNumber) => {
+    console.log("GOT INTO PHONE SIGNIN")
+    // const confirmation = await auth().signInWithPhoneNumber("+1 321-315-1583")
+    // console.log(confirmation)
+    router.push("/phone-sign-in")
+  }
+
+  console.log("HIHI")
+
+  const GoogleIcon = createIcon({
+    viewBox: "0,0,256,256",
+    path: ( <>
+      <Path
+          d="M26 2C13.309 2 3 12.309 3 25s10.309 23 23 23c9.918 0 15.973-4.563 19.125-10.219 3.152-5.656 3.55-12.3 2.594-16.843l-.188-.782h-.781L26 20.125h-1v10.406h11.438c-1.727 4-5.243 6.75-10.438 6.75-6.79 0-12.281-5.492-12.281-12.281 0-6.79 5.492-12.281 12.281-12.281 3.05 0 5.82 1.129 7.969 2.969l.718.593 6.844-6.843.719-.75L41.5 8C37.414 4.277 31.96 2 26 2zm0 2c5.074 0 9.652 1.855 13.281 4.844l-4.812 4.812c-2.38-1.777-5.27-2.937-8.469-2.937-7.871 0-14.281 6.41-14.281 14.281S18.129 39.281 26 39.281c6.55 0 11.262-4.015 12.938-9.468l.406-1.282H27v-6.406l18.844.031c.664 4.035.222 9.828-2.469 14.657C40.515 41.937 35.32 46 26 46 14.387 46 5 36.61 5 25S14.387 4 26 4z"
+          transform="scale(5.12)"
+          fill="#fff"
+          strokeMiterlimit={10}
       />
-      <VStack space="3xl">
-        {/* Heading */}
-        <HStack 
-          className="justify-center items-center" 
-          space="lg" 
-          reversed={false} 
-        >
-          <Image
-            size="lg"
-            source={require('../assets/images/App_Icon.png')}
-            alt="bitebook logo"
-            className="rounded-xl"
-          />
-          <VStack 
-            className="mt-8 lg:mt-3" 
-            space="xs"
+    </>)
+  })
+
+  return (
+      <View
+          className="bg-background-dark px-5 lg:px-40"
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            width: "100%",
+          }}
+      >
+        <LinearGradient
+            colors={[
+              "#232d37", "#232b34", "#222832", "#22262f", "#21242c",
+              "#202229", "#1f2027", "#1e1e24", "#1d1c21", "#1b1a1e",
+              "#1a191c", "#181719"
+            ]}
+            locations={[0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.7]}
+            start={{x: 1, y: 0}}
+            end={{x: 0, y: 1}}
+            style={{
+              position: 'absolute',
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0, // Ensures full height
+              justifyContent: 'center', // Centers content vertically
+              alignItems: 'center', // Centers content horizontally
+            }}
+        />
+        <VStack space="3xl">
+          {/* Heading */}
+          <HStack
+              className="justify-center items-center"
+              space="lg"
+              reversed={false}
           >
-            <Text 
-              className="font-light" 
+            <Image
+                size="lg"
+                source={require('../assets/images/App_Icon.png')}
+                alt="bitebook logo"
+                className="rounded-xl"
+            />
+            <VStack
+                className="mt-8 lg:mt-3"
+                space="xs"
+            >
+              <Text
+                  className="font-light"
               size="4xl">
                 Welcome to
             </Text>
@@ -201,8 +208,10 @@ export default function Login() {
             size="xl" 
             variant="outline" 
             action="primary"
+            onPress={handlePhoneSignIn}
           >
-            <ButtonText>Sign in with Google</ButtonText>
+            <ButtonIcon as={PhoneIcon}></ButtonIcon>
+            <ButtonText>Sign In with Phone</ButtonText>
           </Button>
           <Button 
             className="rounded-xl" 
@@ -210,7 +219,8 @@ export default function Login() {
             variant="outline" 
             action="primary"
           >
-            <ButtonText>Sign in with Apple</ButtonText>
+            <ButtonIcon as={GoogleIcon}></ButtonIcon>
+            <ButtonText>Sign In with Google</ButtonText>
           </Button>
         </VStack>
 
