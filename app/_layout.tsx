20bf2fe03bdfbb10a9d446840f067437bddba70b
skaf.lucas <skaf.lucas.cel@gmail.com>
2025-02-27 23:29:12 -0500

    Add phone sign up page to layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,6 +9,7 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+        <Stack.Screen name="phone-sign-in" options={{ headerShown: false}} />
         <Stack.Screen name="forgot-password" options={{ headerShown: false }} />
         <Stack.Screen name="home" options={{ title: 'BiteBook', headerShown: false }} />
         <Stack.Screen name="recipe" options={{ title: 'Recipe' }} />
