fde549896bcc1e5732bac5a000c0c3d9865c0e48
michelletaing <mleetaing@gmail.com>
2025-03-03 13:26:53 -0500

    updated header styling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,12 +6,26 @@ import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 export default function RootLayout() {
   return (
     <GluestackUIProvider mode="dark">
-      <Stack>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#181719',
+            
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontFamily: 'VVDSRashfield-Normal',
+            fontSize: 20
+          },
+          headerShadowVisible: false,
+          headerBackButtonDisplayMode: 'minimal'
+        }}
+      >
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="sign-up" options={{ headerShown: false }} />
         <Stack.Screen name="forgot-password" options={{ headerShown: false }} />
-        <Stack.Screen name="home" options={{ title: 'BiteBook', headerShown: false }} />
-        <Stack.Screen name="recipe" options={{ title: 'Recipe' }} />
+        <Stack.Screen name="home" options={{ title: 'BiteBook', headerShown: true }} />
+        <Stack.Screen name="recipe" options={{ title: '' }} />
       </Stack>
     </GluestackUIProvider>
   );
