8625b18df6a3b002023ea77d7085fafa84d9e675
michelletaing <mleetaing@gmail.com>
2025-03-03 13:28:41 -0500

    updated background color

--- app/recipe.tsx
+++ app/recipe.tsx
@@ -1,8 +1,11 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { Text } from "@/components/ui/text";
+import { getHeaderTitle } from '@react-navigation/elements';
 
 export default function Recipe() {
   return (
     <View
+      className="bg-background-dark px-5 lg:px-40"
       style={{
         flex: 1,
         justifyContent: "center",
