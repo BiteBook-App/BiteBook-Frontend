798bf5d7e52c6d98e7fbb89b4b4a415e10fb349b
skaf.lucas <skaf.lucas.cel@gmail.com>
2025-03-02 19:44:55 -0500

    Create AuthProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,18 +2,21 @@ import { Stack } from "expo-router";
 
 import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { AuthProvider } from "@/configs/authProvider";
 
 export default function RootLayout() {
   return (
-    <GluestackUIProvider mode="dark">
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="sign-up" options={{ headerShown: false }} />
-        <Stack.Screen name="phone-sign-in" options={{ headerShown: false}} />
-        <Stack.Screen name="forgot-password" options={{ headerShown: false }} />
-        <Stack.Screen name="home" options={{ title: 'BiteBook', headerShown: false }} />
-        <Stack.Screen name="recipe" options={{ title: 'Recipe' }} />
-      </Stack>
-    </GluestackUIProvider>
+      <AuthProvider>
+        <GluestackUIProvider mode="dark">
+          <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+            <Stack.Screen name="phone-sign-in" options={{ headerShown: false}} />
+            <Stack.Screen name="forgot-password" options={{ headerShown: false }} />
+            <Stack.Screen name="home" options={{ title: 'BiteBook', headerShown: false }} />
+            <Stack.Screen name="recipe" options={{ title: 'Recipe' }} />
+          </Stack>
+        </GluestackUIProvider>
+      </AuthProvider>
   );
 }
---
+++ configs/authProvider.js
@@ -0,0 +1,90 @@
+import React, { createContext, useState, useEffect, useContext } from 'react';
+import auth from '@react-native-firebase/auth';
+import {GoogleSignin} from "@react-native-google-signin/google-signin";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    GoogleSignin.configure({
+        webClientId: '41319305564-lkglp2v2m1n55avq36hl5ffm43ptgm5n.apps.googleusercontent.com',
+    });
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    // For Phone Number Method
+    const [confirm, setConfirm] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = auth().onAuthStateChanged(user => {
+            setUser(user);
+            setLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    const value = {
+        user,
+        loading,
+        confirm,
+        login: async (email, password) => {
+            try {
+                await auth().signInWithEmailAndPassword(email, password);
+            } catch (error) {
+                console.error("Login error:", error);
+                throw error;
+            }
+        },
+        signInWithGoogle: async () => {
+            try{
+                await GoogleSignin.hasPlayServices();
+                const response = await GoogleSignin.signIn();
+
+                const googleCredential = auth.GoogleAuthProvider.credential(response.data?.idToken);
+
+                return auth().signInWithCredential(googleCredential)
+            }
+            catch (error) {
+                console.error("Google Sign In error:", error);
+                throw error;
+            }
+        },
+        signInWithPhoneNumber: async (phoneNumber) => {
+            const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
+            setConfirm(confirmation);
+        },
+        confirmPhoneNumberCode: async (code) => {
+            try {
+                await confirm.confirm(code);
+                setConfirm(null);
+            } catch (error) {
+                console.log('Invalid code.');
+            }
+        },
+        register: async (email, password) => {
+            try {
+                await auth().createUserWithEmailAndPassword(email, password);
+            } catch (error) {
+                console.error("Registration error:", error);
+                throw error;
+            }
+        },
+        signOut: async () => {
+            try {
+                await auth().signOut();
+            } catch (error) {
+                console.error("Logout error:", error);
+            }
+        },
+    };
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
