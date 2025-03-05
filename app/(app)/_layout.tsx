e0938dc207d3bec555a121ab9828fdcc62ee462b
michelletaing <mleetaing@gmail.com>
2025-03-05 16:59:57 -0500

    used authprovider to redirect unauthenticated users

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,18 +1,21 @@
 import { Redirect, Stack } from "expo-router";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { useAuth } from "@/configs/authProvider";
 
 export default function AppLayout() {
-  // Handle redirect for non-authenticated users here - this is hard coded for now
-  const user = false;
+  const { user, signOut } = useAuth();
 
-  if (!user) {
+  console.log(user);
+
+  // Unauthenticated users are redirected to login page
+  if (!user)
     return <Redirect href="/(auth)"/>
-  }
 
   return (
     <GluestackUIProvider mode="dark">
       <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)"/>
+        {/* If the user is authenticated, home page renders */}
+        <Stack.Screen name="(tabs)"/> 
       </Stack>
     </GluestackUIProvider>
   );
