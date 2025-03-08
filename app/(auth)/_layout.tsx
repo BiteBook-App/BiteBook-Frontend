a733d8b033bcacbc6a165be9868158b878799e6c
michelletaing <mleetaing@gmail.com>
2025-03-08 14:00:15 -0500

    added back button to subpages

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -6,8 +6,23 @@ import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 export default function AuthLayout() {
   return (
     <GluestackUIProvider mode="dark">
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
+      <Stack screenOptions={{
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontFamily: 'VVDSRashfield-Normal',
+            fontSize: 20
+          },
+          headerShadowVisible: false,
+          headerBackButtonDisplayMode: 'minimal',
+          headerTitle: "",
+          headerTransparent: true
+        }}
+      >
+        <Stack.Screen name="index" options={{ headerShown: false }}/>
+        <Stack.Screen name="forgot-password" />
+        <Stack.Screen name="phone-sign-in" />
+        <Stack.Screen name="phone-sign-up" />
+        <Stack.Screen name="sign-up" />
       </Stack>
     </GluestackUIProvider>
 
