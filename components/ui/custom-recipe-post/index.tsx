44cd599a7749f6ecf07a13d21e6ca22da391fe7d
michelletaing <mleetaing@gmail.com>
2025-03-29 18:47:16 -0400

    created single recipe post on profile page

---
+++ components/ui/custom-recipe-post/index.tsx
@@ -0,0 +1,72 @@
+import { View, Image, StyleSheet } from "react-native";
+import { Text } from "@/components/ui/text";
+import { LinearGradient } from "expo-linear-gradient"; // Import LinearGradient
+import { Button, ButtonText } from "@/components/ui/button";
+import { VStack } from "@/components/ui/vstack";
+import { HStack } from "@/components/ui/hstack";
+
+interface RecipePost {
+  photoUrl: string,
+  mealName: string,
+  tastes: string[]
+}
+
+export default function RecipePost({photoUrl, mealName, tastes}: RecipePost) {
+  return (
+    <VStack space="3xl">
+      <View style={styles.container}>
+        <Image
+          source={{ uri: `${photoUrl}` }}
+          style={styles.image}
+          alt="recipe post"
+        />
+        <LinearGradient
+          colors={["transparent", "rgba(53, 5, 5, 0.5)"]} // Gradient from transparent to darker
+          style={styles.gradient}
+        />
+        <HStack space="sm" style={styles.buttonContainer} className="m-2">
+          {tastes.map((taste, index) => (
+            <Button key={index} style={styles.taste} className="rounded-full" variant="solid" size="sm">
+              <ButtonText>{taste}</ButtonText>
+            </Button>
+          ))}
+        </HStack>
+        <Text style={styles.overlayText} bold={true} size="3xl" className="m-6">{ mealName }</Text>
+      </View>
+    </VStack>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: "relative", // Ensures the text can be positioned within the container
+  },
+  image: {
+    width: "100%",
+    height: 350,
+    borderRadius: 10,
+  },
+  overlayText: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    color: "white",
+  },
+  gradient: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: "50%", // Adjust height of the blur area
+    borderRadius: 10,
+  },
+  taste: {
+
+  },
+  buttonContainer: {
+    position: "absolute",
+    top: 0,
+    right: 0
+  }
+});
