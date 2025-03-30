2adcc7acb5fc079cd1030b50f423017ffd8c1da3
michelletaing <mleetaing@gmail.com>
2025-03-30 09:54:00 -0400

    added settings page

--- app/(app)/(tabs)/(profile)/settings.tsx
+++ app/(app)/(tabs)/(profile)/settings.tsx
@@ -1,16 +1,31 @@
 import { View } from "react-native";
 import { Text } from "@/components/ui/text";
+import { VStack } from "@/components/ui/vstack";
+import { useAuth } from "@/configs/authProvider";
+import { Pressable } from "@/components/ui/pressable";
 
 export default function Settings() {
+  const { signOut, deleteUser } = useAuth();
+
   return (
     <View
       className="bg-background-dark px-5 lg:px-40"
       style={{
         flex: 1,
-        justifyContent: "center",
       }}
     >
-      <Text>This is the settings page</Text>
+      <VStack className="mt-5">
+        <Text className="font-[Rashfield] leading-[69px] lg:leading-[55px]" size="5xl">
+          Settings
+        </Text>
+        <Text size="xl">Edit Profile</Text>
+        <Pressable onPress={async () => await signOut()}>
+          <Text size="xl">Sign Out</Text>
+        </Pressable>
+        <Pressable onPress={async () => await deleteUser()}>
+          <Text size="xl">Delete Account</Text>
+        </Pressable>      
+      </VStack>
     </View>
   );
 }
