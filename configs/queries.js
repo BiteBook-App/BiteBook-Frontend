a4df067e532b75764a5c913f09ea122a39009ce9
michelletaing <mleetaing@gmail.com>
2025-03-30 11:39:18 -0400

    added gql recipe and profile queries

--- configs/queries.js
+++ configs/queries.js
@@ -2,31 +2,52 @@ import {gql} from '@apollo/client'
 
 export const GET_RECIPES = gql`
   query GetRecipe {
-      getRecipe(recipeUid: "OA3NkEzSqFdKM26Hvf1x") {
-        userId
-        uid
-        url
-        name
-        photoUrl
-        likes
-        createdAt
-        steps {
-          expanded
-          text
-        }
-        ingredients {
-          count
-          name
-        }
-        tastes
+    getRecipe(recipeUid: "OA3NkEzSqFdKM26Hvf1x") {
+      userId
+      uid
+      url
+      name
+      photoUrl
+      likes
+      createdAt
+      steps {
+        expanded
+        text
       }
+      ingredients {
+        count
+        name
+      }
+      tastes
+    }
+  }
+`;
+
+export const GET_RECIPE_PREVIEW = gql`
+  query GetRecipePreview($userId: String) {
+    getRecipes(userId: $userId) {
+      createdAt
+      name
+      photoUrl
+      tastes
+      uid
+    }
+  }
+`;
+
+export const GET_PROFILE = gql`
+  query MyQuery($uid: String) {
+    getUsers(uid: $uid) {
+      displayName
+      profilePicture
+    }
   }
 `;
 
 export const EDIT_RECIPE = gql`
-    mutation EditRecipe($recipeData: RecipeInput!) {
-        editRecipe(recipeData: $recipeData, recipeId: "OA3NkEzSqFdKM26Hvf1x") {
-            uid
-        }
-   }
+  mutation EditRecipe($recipeData: RecipeInput!) {
+      editRecipe(recipeData: $recipeData, recipeId: "OA3NkEzSqFdKM26Hvf1x") {
+          uid
+      }
+  }
 `;
