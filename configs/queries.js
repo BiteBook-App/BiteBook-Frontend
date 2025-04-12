ebedc24123a2cd51fed70beda8b1d08e2eacb618
michelletaing <mleetaing@gmail.com>
2025-04-12 17:34:22 -0400

    return recipe id for home page posts and retrieve user info for a recipe

--- configs/queries.js
+++ configs/queries.js
@@ -118,9 +118,23 @@ export const GET_HOME_PAGE = gql`
       likes
       tastes
       createdAt
+      uid
       user {
         displayName
         profilePicture
+      }
+    }
+  }
+`;
+
+export const GET_USER_FROM_RECIPE = gql`
+  query GetRecipe($recipeUid: String!) {
+    getRecipe(recipeUid: $recipeUid) {
+      uid
+      user {
+        displayName
+        profilePicture
+        uid
       }
     }
   }
