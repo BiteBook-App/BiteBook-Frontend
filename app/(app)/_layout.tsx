2b0adb5445e329a09cbf5505eedf4e0126ea18c5
skaf.lucas <skaf.lucas.cel@gmail.com>
2025-04-13 14:55:04 -0400

    Handle url input in the app

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,13 +1,28 @@
-import { Redirect, Stack } from "expo-router";
+import {Redirect, router, Stack} from "expo-router";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { useAuth } from "@/configs/authProvider";
+import {useURL, parse} from 'expo-linking'
 
 export default function AppLayout() {
   const { user, signOut } = useAuth();
+  const url = useURL();
+
+  console.log("TEST " + url);
   
   // Unauthenticated users are redirected to login page
   if (!user)
     return <Redirect href="/(auth)"/>
+
+  if (url) {
+      const parsedURL = parse(url);
+
+      const actionPage = parsedURL.path;
+      const actionID = parsedURL.queryParams?.id;
+
+      if (actionPage == "friends") {
+          router.push( { pathname: '/(app)/(tabs)/(friends)', params: { id: actionID } })
+      }
+  }
 
   return (
     <GluestackUIProvider mode="dark">
