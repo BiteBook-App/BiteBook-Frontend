295f5521198be53a6512ba4bebf20cab702f5e41
michelletaing <mleetaing@gmail.com>
2025-04-13 13:35:23 -0400

    initialized taste page

--- app/(app)/(tabs)/(taste)/index.tsx
+++ app/(app)/(tabs)/(taste)/index.tsx
@@ -1,16 +1,29 @@
 import { View } from "react-native";
 import { Text } from "@/components/ui/text";
+import { useAuth } from "@/configs/authProvider";
+import { useQuery } from "@apollo/client";
+import { GET_PROFILE } from "@/configs/queries";
 
 export default function Taste() {
+  const { user } = useAuth();
+
+  const { loading: profileLoading, error: profileError, data: profile, refetch: refetchProfile } = useQuery(GET_PROFILE, {
+    variables: { uid: user?.uid }, // Ensure the userId is passed correctly
+    skip: !user?.uid, // Prevents running query if user.uid is undefined
+  });
+
   return (
     <View
       className="bg-background-dark px-5 lg:px-40"
       style={{
         flex: 1,
-        justifyContent: "center",
       }}
     >
-      <Text>This is the taste page</Text>
+      <Text className="font-[Rashfield] mt-5 pt-8" size="5xl">
+        Hi, {profile?.getUsers?.[0]?.displayName}!
+      </Text> 
+
+      <Text size="xl">The tastes you enjoyed the most this month</Text>
     </View>
   );
 }
