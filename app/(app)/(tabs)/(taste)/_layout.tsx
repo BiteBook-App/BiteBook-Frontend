cbfde1ed663d22b427dd5a23c1ea64172df1465a
michelletaing <mleetaing@gmail.com>
2025-04-18 10:47:38 -0400

    update layout based to add new pages

--- app/(app)/(tabs)/(taste)/_layout.tsx
+++ app/(app)/(tabs)/(taste)/_layout.tsx
@@ -15,6 +15,11 @@ export default function AppLayout() {
         }}
       >
         <Stack.Screen name="index" options={{ headerShown: true, headerBackButtonDisplayMode: 'minimal', headerTitle: 'BiteBook'}}/>
+        <Stack.Screen name="[recipeId]" options={{ headerShown: true, headerBackButtonDisplayMode: 'minimal', headerTitle: ''}}/>
+        <Stack.Screen name="(friend)/[userId]" options={{ headerShown: true, headerBackButtonDisplayMode: 'minimal', headerTitle: ''}}/>
+        <Stack.Screen name="(friend)/(recipe)/[recipeId]" options={{ headerShown: true, headerBackButtonDisplayMode: 'minimal', headerTitle: ''}}/>
+
+
       </Stack>
     </GluestackUIProvider>
   );
