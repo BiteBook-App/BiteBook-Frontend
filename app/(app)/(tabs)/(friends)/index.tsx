06eedc2f609682ac383b628ddac73ab7b0dd6ace
skaf.lucas <skaf.lucas.cel@gmail.com>
2025-04-19 19:29:12 -0400

    Hot Fix: Remove error toast when closing share

--- app/(app)/(tabs)/(friends)/index.tsx
+++ app/(app)/(tabs)/(friends)/index.tsx
@@ -143,8 +143,7 @@ export default function Friends() {
         })
 
         Share.open({message: "Invite friends to BiteBook!", title: "BiteBook Invitation", url: invitationURL})
-            .then((res) => handleToast('success', "Link was copied to your clipboard!"))
-            .catch((err) => handleToast('error', "Please try again later."));
+            .then((res) => handleToast('success', "Link was copied to your clipboard!"));
     }
 
     // The search function that filters by name or username - Fixed for React Native
